d73bc3e30d61db27d8134851cb70f1f33c9cb06f
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-09-23 23:11:46 +0100

    Basic setup

---
+++ src/Game/Osu/FreeTaiko.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE UnicodeSyntax #-}
+{-# OPTIONS_HADDOCK show-extensions #-}
+
+-- |
+-- Module      :  Game.Osu.FreeTaiko
+-- License     :  GPL-3
+-- Copyright   :  © Mateusz Kowalczyk, 2014
+-- Maintainer  :  fuuzetsu@fuuzetsu.co.uk
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Central point unifying various free-taiko components.
+
+module Game.Osu.FreeTaiko where
---
+++ src/Main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE UnicodeSyntax #-}
+{-# OPTIONS_HADDOCK show-extensions #-}
+
+-- |
+-- Module      :  Main
+-- License     :  GPL-3
+-- Copyright   :  © Mateusz Kowalczyk, 2014
+-- Maintainer  :  fuuzetsu@fuuzetsu.co.uk
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Main entry point to free-taiko
+
+module Main where
+
+main ∷ IO ()
+main = return ()
